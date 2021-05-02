4655387bc5f071d96d7b2eac158eefe94221bd1c
pahaeussler <pahaeussler@uc.cl>
2021-05-02 16:16:30 -0400

    Run in diferents environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA_eTkekCPcKSBlauJa27UcDuTb3U8TKz0',
+    authDomain: 'yambe-dev.firebaseapp.com',
+    projectId: 'yambe-dev',
+    storageBucket: 'yambe-dev.appspot.com',
+    messagingSenderId: '669992930162',
+    appId: '1:669992930162:web:75c5536d85bce19b782beb',
+    measurementId: 'G-S6GCC7ZMF0'
+  }
 };
