3d30208badb9404c732f720563ca5859ea05e8fd
ignaciohermosillacornejo <hermosillaignacio@gmail.com>
2021-06-14 02:10:33 -0400

    suffle cards when loading deck

--- functions/src/game/helper/load-card-deck.ts
+++ functions/src/game/helper/load-card-deck.ts
@@ -1,5 +1,22 @@
 import { firestore } from 'firebase-admin';
 
+const shuffle = (array: any[]) => {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
+};
+
 /**
  * Fetches the requested deck from the Cards collections
  * and copies it to the deck subcollection in the game
@@ -8,11 +25,14 @@ import { firestore } from 'firebase-admin';
 const loadDeck = async (data: { gameId: string, deckChoice: string}) => {
   const { gameId, deckChoice = 'default' } = data;
 
-  const cards = await firestore()
+  const cardsSnap = await firestore()
     .collection('decks')
     .doc(deckChoice)
     .collection('cards')
     .get();
+
+  const cards = cardsSnap.docs;
+  shuffle(cards);
 
   cards.forEach(async (doc) => {
     await firestore().collection('games').doc(gameId).collection('deck')
