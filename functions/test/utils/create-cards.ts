2da9116ce95205a853bf2880d682f3ca15c8f79f
ignaciohermosillacornejo <hermosillaignacio@gmail.com>
2021-06-20 02:43:02 -0400

    added card factory

---
+++ functions/test/utils/create-cards.ts
@@ -0,0 +1,35 @@
+function* indexGenerator() {
+  let startIndex = 0;
+  while (true) {
+    yield startIndex;
+    startIndex += 1;
+  }
+}
+
+const createCards = (numberOfAnswerCards: number, numberOfQuestionCards: number) => {
+  const id = indexGenerator();
+  const newAnswerCards = new Array(numberOfAnswerCards)
+    .fill(null)
+    .map(() => {
+      const cardId = id.next().value;
+      return {
+        id: `${cardId}`,
+        type: 'answer',
+        content: `example answer ${cardId}`,
+      };
+    });
+
+  const newQuestionCards = new Array(numberOfQuestionCards)
+    .fill(null)
+    .map(() => {
+      const cardId = id.next().value;
+      return {
+        id: `${cardId}`,
+        type: 'question',
+        content: `_ is example question ${cardId}`,
+      };
+    });
+  return { newAnswerCards, newQuestionCards };
+};
+
+export { createCards };
